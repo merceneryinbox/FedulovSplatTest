88183299bd7d215c2d706a355fef0aaaaaaaef9a
mercenery <mercenery@inbox.ru>
2017-07-14 23:56:41 +0300

    Splat test v 1.0 working

---
+++ src/sample/AddDialogController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by mercenery on 14.07.2017.
+ */
+public class AddDialogController {
+    @FXML
+    Button okButton;
+
+    @FXML
+    TextField inptTxtInAddingDialog;
+
+    public void modalWindowAddFolder(ActionEvent actionEvent) {
+
+    }
+
+    public void modalWindowAddClose(ActionEvent actionEvent) {
+
+    }
+}
